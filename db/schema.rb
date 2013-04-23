861044e9e52c6c9fd34ba6a0713c7aa600baa9e9
Sean Wu <wushuang88@gmail.com>
2013-04-23 18:30:03 +0800

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130412160621) do
+ActiveRecord::Schema.define(:version => 20130412161853) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Sean Wu"
+    email    "wushuang88@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
