ca17502c9d9d8d55961e2b2f628d8831e613f9bf
Sean Wu <wushuang88@gmail.com>
2013-05-09 17:17:08 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -47,17 +48,11 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_path, notice: "Please sign in."
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-  end
+  
 
   def admin_user
       redirect_to(root_path) unless current_user.admin?
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,34 +28,43 @@ describe "User pages" do
         end 
       end
     end
-  end
+  
 
-  describe "delete links" do
+    describe "delete links" do
 
-    it { should_not have_link('delete') }
+      it { should_not have_link('delete') }
 
-    describe "as an admin user" do
-      let(:admin) { FactoryGirl.create(:admin) }
-      before do
-        sign_in admin
-        visit users_path
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
-
-      it { should have_link('delete', href: user_path(User.first)) }
-      it "should be able to delete another user" do
-        expect { click_link('delete') }.to change(User, :count).by(-1)
-      end
-      it { should_not have_link('delete', href: user_path(admin)) }
     end
   end
-  
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup page" do
